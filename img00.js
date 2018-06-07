ab966622043cf98a19ab6c8df9d3452437bca539
Bohyung Kim <dsdgun@gmail.com>
2018-06-07 10:18:07 +0900

    extract model update function

--- img00.js
+++ img00.js
@@ -51,119 +51,123 @@ let w = canvas.clientWidth, h = canvas.clientHeight;
 // projection * translate * rot*scale*position
 
 let models = (() => {
-    var arr = []
-    for (let i = 0, n =1000; i<n; i++) arr.push({p: {x:w * Math.random(), y: h * Math.random()},
-                                                scale:Math.random() * 0.1,
-                                                angle:Math.random() * 360,
-                                                speed: 2,
-                                                center() {
-                                                    return {x: image.width * this.scale * 0.5, y:image.height * this.scale * 0.5}
-                                                }})
-                                                
-    return arr
+  var arr = []
+  for (let i = 0, n =1000; i<n; i++) arr.push({p: {x:w * Math.random(), y: h * Math.random()},
+                                               scale:Math.random() * 0.1,
+                                               angle:Math.random() * 360,
+                                               speed: 2,
+                                               center() {
+                                                 return {x: image.width * this.scale * 0.5, y:image.height * this.scale * 0.5}
+                                               },
+                                               update() {
+                                                 this.angle += this.speed
+                                               }})
+  
+  return arr
 })()
 let image;
 loadTexture('t0.png').then(function(img) {
-    image = img;
-    render();
+  image = img;
+  render();
 });
 var angle = 1;
 function render() {
-    resizeCanvasToDisplaySize(gl.canvas);
+  resizeCanvasToDisplaySize(gl.canvas);
 
-    gl.useProgram(program);
+  gl.useProgram(program);
 
-    var positionAttributeLocation = gl.getAttribLocation(program, 'a_position');
-    var texCoordAttributeLocation = gl.getAttribLocation(program, 'a_texCoord');
-    var matLocation = gl.getUniformLocation(program, 'u_matrix');
-    var imgLocation = gl.getUniformLocation(program, 'u_image');
+  var positionAttributeLocation = gl.getAttribLocation(program, 'a_position');
+  var texCoordAttributeLocation = gl.getAttribLocation(program, 'a_texCoord');
+  var matLocation = gl.getUniformLocation(program, 'u_matrix');
+  var imgLocation = gl.getUniformLocation(program, 'u_image');
 
-    var kernelLocation = gl.getUniformLocation(program, 'u_kernel[0]');
-    var kernelWeightLocation = gl.getUniformLocation(program, 'u_kernelWeight');
+  var kernelLocation = gl.getUniformLocation(program, 'u_kernel[0]');
+  var kernelWeightLocation = gl.getUniformLocation(program, 'u_kernelWeight');
 
-    var edgeDetectKernel = [
-        -1, -1, -1,
-        -1, 8, -1,
-        -1,-1,-1
-    ];
+  var edgeDetectKernel = [
+    -1, -1, -1,
+    -1, 8, -1,
+    -1,-1,-1
+  ];
 
-    var vao = gl.createVertexArray();
-    gl.bindVertexArray(vao);
+  var vao = gl.createVertexArray();
+  gl.bindVertexArray(vao);
 
-    var positionBuffer = gl.createBuffer();
-    gl.enableVertexAttribArray(positionAttributeLocation);
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  var positionBuffer = gl.createBuffer();
+  gl.enableVertexAttribArray(positionAttributeLocation);
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
-    gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0);
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0);
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
-    setRect(gl, 0, 0, image.width, image.height);
+  setRect(gl, 0, 0, image.width, image.height);
 
-    var texCoordBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, texCoordBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
-        0.0,0.0,
-        1.0,0.0,
-        0.0,1.0,
-        0.0,1.0,
-        1.0,0.0,
-        1.0,1.0
-    ]), gl.STATIC_DRAW);
-    gl.enableVertexAttribArray(texCoordAttributeLocation);
+  var texCoordBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, texCoordBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+    0.0,0.0,
+    1.0,0.0,
+    0.0,1.0,
+    0.0,1.0,
+    1.0,0.0,
+    1.0,1.0
+  ]), gl.STATIC_DRAW);
+  gl.enableVertexAttribArray(texCoordAttributeLocation);
 
-    var size=2,
-        type=gl.FLOAT,
-        normalize=false,
-        stride=0,
-        offset=0;
-    gl.vertexAttribPointer(texCoordAttributeLocation, size,type,normalize,stride,offset);
+  var size=2,
+      type=gl.FLOAT,
+      normalize=false,
+      stride=0,
+      offset=0;
+  gl.vertexAttribPointer(texCoordAttributeLocation, size,type,normalize,stride,offset);
 
-    var texture = createAndSetupTexture(gl);
-    var mipLevel = 0,
-        internalFormat = gl.RGBA,
-        srcFormat = gl.RGBA,
-        srcType = gl.UNSIGNED_BYTE;
-    gl.texImage2D(gl.TEXTURE_2D,
-                  mipLevel,
-                  internalFormat,
-                  srcFormat,
-                  srcType,
-                  image);
+  var texture = createAndSetupTexture(gl);
+  var mipLevel = 0,
+      internalFormat = gl.RGBA,
+      srcFormat = gl.RGBA,
+      srcType = gl.UNSIGNED_BYTE;
+  gl.texImage2D(gl.TEXTURE_2D,
+                mipLevel,
+                internalFormat,
+                srcFormat,
+                srcType,
+                image);
 
-    gl.bindVertexArray(vao);
+  gl.bindVertexArray(vao);
 
-    // 뷰포트 
-    gl.viewport(0, 0, gl.canvas.clientWidth, gl.canvas.clientHeight);
+  // 뷰포트 
+  gl.viewport(0, 0, gl.canvas.clientWidth, gl.canvas.clientHeight);
 
-    // canvas 클리어 
-    gl.clearColor(0,0,0,0);
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  // canvas 클리어 
+  gl.clearColor(0,0,0,0);
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
 
-    // 이미지를 texture 0 번에 할당 
-    gl.uniform1i(imgLocation, 0);
+  // 이미지를 texture 0 번에 할당 
+  gl.uniform1i(imgLocation, 0);
 
-    gl.uniform1fv(kernelLocation, edgeDetectKernel);
-    gl.uniform1f(kernelWeightLocation, computeKernelWeight(edgeDetectKernel));
-    models.forEach((model, index) => {
-        model.angle += model.speed
-        let m = m3.projection(w, h);
-        m = m3.translate(m, model.p.x, model.p.y);
-        m = m3.translate(m, -model.center().x, -model.center().y);
-        m = m3.rotate(m, rad(model.angle));
-        m = m3.translate(m, -model.center().x, -model.center().y);
-        m = m3.scale(m, model.scale, model.scale);
-        gl.uniformMatrix3fv(matLocation, false, m);
-        gl.drawArrays(gl.TRIANGLES, 0, 6);
-    })
-    requestAnimationFrame(render);
+  gl.uniform1fv(kernelLocation, edgeDetectKernel);
+  gl.uniform1f(kernelWeightLocation, computeKernelWeight(edgeDetectKernel));
+  models.forEach((model, index) => {
+    model.update()
+    // world viewport
+    let m = m3.projection(w, h);
+    m = m3.translate(m, model.p.x, model.p.y);
+    m = m3.translate(m, -model.center().x, -model.center().y);
+    m = m3.rotate(m, rad(model.angle));
+    m = m3.translate(m, -model.center().x, -model.center().y);
+    m = m3.scale(m, model.scale, model.scale);
+    gl.uniformMatrix3fv(matLocation, false, m);
+    gl.drawArrays(gl.TRIANGLES, 0, 6);
+  })
+  requestAnimationFrame(render);
 }
 
 function computeKernelWeight(kernel) {
-    var weight = kernel.reduce((p, c) => {
-        return p + c;
-    });
-    return weight <= 0 ? 1 : weight;
+  var weight = kernel.reduce((p, c) => {
+    return p + c;
+  });
+  return weight <= 0 ? 1 : weight;
 }
 
 function resize(canvas) {
